81ea0747ae0f9c2c9715e16ac852e82ac7a5a5bc
siddhantdev <agarwalsiddhant53@gmail.com>
2023-08-12 03:42:08 +0530

    Added option to change size of the grid

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 const grid = document.querySelector('.grid');
 const colorPicker = document.querySelector('#favcolor');
 
-let n = 8;
-let size = 800/n - 2;
-let pixels = [];
-
 let mouseDown = false;
-
 document.body.onmousedown = () => mouseDown = true;
 document.body.onmouseup = () => mouseDown = false;
 
-for(let i = 0; i < n * n; ++i) {
-    const pixel = document.createElement('div');
-    pixel.classList.add('pixel')
-    pixel.style.flex = `0 1 ${size}px`;
-    pixels.push(pixel);
+let pixels = [];
+
+function createGrid(n = 8) {
+    let size = 800/n - 2;
+    pixels = [];
+
+    grid.replaceChildren();
+
+    for(let i = 0; i < n * n; ++i) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel')
+        pixel.style.flex = `0 1 ${size}px`;
+        pixels.push(pixel);
+    }
+
+    pixels.forEach(pixel => pixel.addEventListener('mousedown', fill));
+    pixels.forEach(pixel => pixel.addEventListener('mouseover', fill));
+
+    pixels.forEach(pixel => grid.appendChild(pixel));
 }
-
-pixels.forEach(pixel => grid.appendChild(pixel));
-
-pixels.forEach(pixel => pixel.addEventListener('mousedown', fill));
-pixels.forEach(pixel => pixel.addEventListener('mouseover', fill));
+createGrid(8);
 
 function fill(e) {
     if(e.type === 'mouseover' && !mouseDown) return;
@@ -32,4 +37,10 @@ function fill(e) {
 const clear_btn = document.querySelector('.clear');
 clear_btn.addEventListener('click', () => {
     pixels.forEach(pixel => pixel.style.background = "white");
+});
+
+const sz_btn = document.querySelector('.size');
+sz_btn.addEventListener('click', () => {
+    let n = prompt("Enter the side of each side");
+    createGrid(n);
 });
