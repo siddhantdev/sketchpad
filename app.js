d3888c59debd84e022c4aae74df512dcbc4f6e78
siddhantdev <agarwalsiddhant53@gmail.com>
2023-08-12 01:58:05 +0530

    Added some basic functionality

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const grid = document.querySelector('.grid');
 
+let brush = "black";
+
 let n = 8;
 let size = 800/n - 2;
 let pixels = [];
@@ -12,3 +14,17 @@ for(let i = 0; i < n * n; ++i) {
 }
 
 pixels.forEach(pixel => grid.appendChild(pixel));
+
+pixels.forEach(pixel => pixel.addEventListener('mouseover', () => {
+    pixel.style.background = brush;
+}))
+
+const erase_btn = document.querySelector('.eraser');
+erase_btn.addEventListener('click', () => {
+    brush = "white";
+});
+
+const clear_btn = document.querySelector('.clear');
+clear_btn.addEventListener('click', () => {
+    pixels.forEach(pixel => pixel.style.background = "white");
+});
